491e8764cf01d423c5df55d78547efea1a5f2b12
Seiichi Uchida <topecongiro@fastmail.com>
2025-04-12 07:47:52 +0900

    Add missing IOSQE macro definitions

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+pub const IOSQE_FIXED_FILE: u32 = 1 << IOSQE_FIXED_FILE_BIT;
+pub const IOSQE_IO_DRAIN: u32 = 1 << IOSQE_IO_DRAIN_BIT;
+pub const IOSQE_IO_LINK: u32 = 1 << IOSQE_IO_LINK_BIT;
+pub const IOSQE_IO_HARDLINK: u32 = 1 << IOSQE_IO_HARDLINK_BIT;
+pub const IOSQE_ASYNC: u32 = 1 << IOSQE_ASYNC_BIT;
+pub const IOSQE_BUFFER_SELECT: u32 = 1 << IOSQE_BUFFER_SELECT_BIT;
+pub const IOSQE_CQE_SKIP_SUCCESS: u32 = 1 << IOSQE_CQE_SKIP_SUCCESS_BIT;
+
 #[cfg(test)]
 mod test {
     use std::{
